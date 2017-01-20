aefabfcb8f7b91b5cd0ba35bbb9408034b4c6b82
KemalSoylu <kemalbsoylu@gmail.com>
2017-01-20 14:38:04 +0300

    html ve içerik düzenlemeleri

--- places/models.py
+++ places/models.py
@@ -24,14 +24,14 @@ class PlaceManager(models.Manager):
 
 class Place(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    name = models.CharField(max_length=255)
+    name = models.CharField('Mekan Adı', max_length=255)
     is_active = models.BooleanField(default=False)
     coordinates = models.CharField(max_length=255, blank=False, null=True)
     category = models.ForeignKey(Category, blank=True, null=True)
-    has_wifi = models.BooleanField(default=False)
-    telephone = models.CharField(max_length=255, blank=True, null=True)
-    web_site = models.CharField(max_length=255, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    has_wifi = models.BooleanField('Wifi?', default=False)
+    telephone = models.CharField('Telefon', max_length=255, blank=True, null=True)
+    web_site = models.CharField('Web sitesi', max_length=255, blank=True, null=True)
+    description = models.TextField('Açıklama', blank=True, null=True)
 
     objects = PlaceManager()
     all_objects = models.Manager()
@@ -49,8 +49,9 @@ class Place(models.Model):
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     place = models.ForeignKey(Place)
-    comment = models.TextField(blank=True, null=True)
+    comment = models.TextField('Yorum', blank=False, null=True)
     vote = models.IntegerField(
+        'Oylama',
         default=3,
         choices=(
             (1, 'Berbat'),
@@ -66,7 +67,7 @@ class Review(models.Model):
 
 class Media(models.Model):
     place = models.ForeignKey(Place)
-    image = models.ImageField(upload_to="places")
+    image = models.ImageField('Fotoğraf', upload_to="places")
 
     class Meta:
         verbose_name_plural = "Media"
